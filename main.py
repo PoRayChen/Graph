459c642a787ad1cd5e60308b06fb7dcf0047d0ac
PoRayChen <57026449+PoRayChen@users.noreply.github.com>
2021-05-02 18:10:45 -0400

    F

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import matplotlib as plt
-Month = ()
-High = [44.91, 58.09, 78.07, 107.7, 138.5, 170.6]
-Low = [449.48, 553.57, 696.783, 870.133, 1000.
-             4, 1309.1]
+import matplotlib.pyplot as plt
+Month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec')
+High = [38, 41, 49.8, 60.7, 70.9, 79, 84.2, 82.4, 74.7, 63.5, 53.1, 42.9]
+Low = [26.2, 28.1, 35.1, 44.2, 54.2, 63.3, 68.8, 67.7, 60.3, 49.6, 41, 31.6]
 
 plt.plot(Month, High, color='red')
 plt.plot(Month, Low, color='blue')
 plt.xlabel('Month')
 plt.ylabel('Tempture')
-plt.title('Pakistan India Population till 2010')
+plt.title('NYC tempture')
 plt.show()
